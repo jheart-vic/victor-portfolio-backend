7201466e73e40ed96fd62cdcee458b89bb053a7d
jheart-vic <adebowalevictorjheart@gmail.com>
2022-11-23 14:55:53 +0100

    Update heroku version

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'api/v1/projects#index'
+  # root 'api/v1/projects#index'
   namespace :api do
     namespace :v1 do
-      resources :projects
+      resources :projects, only: %i[index]
     end
   end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
