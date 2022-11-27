0cec4d57555795c7ac06318cbcf28984dd432209
jheart-vic <adebowalevictorjheart@gmail.com>
2022-11-27 22:52:51 +0100

    Update image

--- db/seeds.rb
+++ db/seeds.rb
@@ -13,17 +13,17 @@ Project.create(
   image: 'https://res.cloudinary.com/dtokddgbm/image/upload/v1669583154/samples/portfolio/spacetravel_rw2t2u.jpg',
   description: "This is an app that displays a list of Rockets and Missions, allows users to reserve and cancel a Rocket, and joins and leaves a Mission,
   It fetches data from an API and then sends it to the browser for users to view",
-  technologies_stack: %w[css html bootstrap Ruby],
-  live_link: 'https://jheart-vic.github.io/My-Portfolio-Website/',
-  source_link: 'https://github.com/jheart-vic/My-Portfolio-Website'
+  technologies_stack: %w[Redux React Bootstrap],
+  live_link: 'https://maenkhraisha.github.io/space-travel/',
+  source_link: 'https://github.com/jheart-vic/space-travel'
 )
 Project.create(
   title: 'Movie-List App',
   image: 'https://res.cloudinary.com/dtokddgbm/image/upload/v1669583650/samples/portfolio/Screenshot_113_mqjrvk.png',
-  description: "MOVIE DISPLAY is an App that gets a list of movies from the server and displays it for the user to select their choice, This app helps you with movies to watch if you are looking for one ",
-  technologies_stack: %w[html bootstrap Ruby],
-  live_link: 'https://jheart-vic.github.io/My-Portfolio-Website/',
-  source_link: 'https://github.com/jheart-vic/My-Portfolio-Website'
+  description: "Movie display is an App that gets a list of movies from the server and displays it for the user to select their choice, This app helps you with movies to watch if you are looking for one ",
+  technologies_stack: %w[React Bootstrap CSS],
+  live_link: 'https://jheart-vic.github.io/movie-display/',
+  source_link: 'https://github.com/jheart-vic/movie-display'
 )
 Project.create(
   title: 'Book-Store App',
@@ -38,7 +38,7 @@ Project.create(
   title: 'Budget-Keeping App',
   image: 'https://res.cloudinary.com/dtokddgbm/image/upload/v1669562883/samples/portfolio/Screenshot_112_gsgmmt.png',
   description: 'This is a full-stack App that allows users to sign up and have their budget and expenses saved into a database, it uses Postgresql on the server side and ruby on the client side, and users can delete their budgets',
-  technologies_stack: %w[html bootstrap Ruby],
+  technologies_stack: %w[Html Bootstrap Ruby],
   live_link: 'https://budgetory.herokuapp.com/',
   source_link: 'https://github.com/jheart-vic/Budget-App'
 )
@@ -56,7 +56,7 @@ Project.create(
   title: 'Healthy-Diet App',
   image: 'https://res.cloudinary.com/dtokddgbm/image/upload/v1669562883/samples/portfolio/Screenshot_21_egvvmu.png',
   description: 'This is a balanced diet site that helps you know how to eat right and what to eat at the right time, it is a simple App built with JavaScript, Html, and CSS',
-  technologies_stack: %w[HTML CSS JavaScript],
+  technologies_stack: %w[Html CSS JavaScript],
   live_link: 'https://jheart-vic.github.io/capstone/',
   source_link: 'https://github.com/jheart-vic'
 )
